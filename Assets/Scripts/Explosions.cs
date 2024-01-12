9da9f3af7d87c435aa7142bb63ebdcd776bcfd1a
ducanh040102 <khoaimon3570@gmail.com>
2024-01-12 20:20:05 +0700

    small-value-change

--- Assets/Scripts/Explosions.cs
+++ Assets/Scripts/Explosions.cs
@@ -11,7 +11,7 @@ public class Explosions : MonoBehaviour
 
     private IEnumerator Despawn()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1.5f);
         gameObject.gameObject.SetActive(false);
     }
 
